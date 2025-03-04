136c0fb5674f7f4ec3ff1edd3a1fbea5c05d939c
LouConreux <louis.conreux1@gmail.com>
2025-03-04 10:26:29 -0800

    [edit] Add noisy computation of profile

--- compute_profile.py
+++ compute_profile.py
@@ -29,10 +29,15 @@ def main(args):
     qmax = exp_profile.max_q_
     delta_q = exp_profile.delta_q_
     ff_type = FormFactorType.ALL_ATOMS    
-    profile = compute_profile(particles=particles, min_q=qmin, max_q=qmax, delta_q=delta_q, ff_type=ff_type, gpu=gpu)
+    profile = compute_profile(particles=particles, min_q=qmin, max_q=qmax, delta_q=delta_q, ff_type=ff_type)
     model_profile, chi_square, fitted_params = fit_profile(exp_profile, profile, min_c1=0.9, max_c1=1.2, min_c2=-2.0, max_c2=4.0, use_offset=False)
     file_name = f"{args.pdb.split('/')[-1].split('.')[0]}_profile.png"
     plot_SAXS_profile(model_profile, exp_profile, chi_square, fitted_params, os.path.join(args.outdir, file_name))
+    model_profile.write_SAXS_file(os.path.join(args.outdir, f"{args.pdb.split('/')[-1].split('.')[0]}_model_profile.dat"))
+    intensity_model = model_profile.intensity_
+    noisy_intensity = intensity_model + args.eps * np.random.randn(len(intensity_model))
+    model_profile.intensity_ = noisy_intensity
+    model_profile.write_SAXS_file(os.path.join(args.outdir, f"{args.pdb.split('/')[-1].split('.')[0]}_noisy_model_profile.dat"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -41,6 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("--outdir", type=str, required=True, help="Path to output directory.")
     parser.add_argument("--dat", type=str, required=True, help="Path to the SAXS data file.")
     parser.add_argument("--pdb", type=str, required=True, help="Path to deposited PDB file.")
+    parser.add_argument("--eps", type=float, default=0.1, help="Epsilon noise value for the SAXS profile computation.")
     
     args = parser.parse_args()
     main(args)
